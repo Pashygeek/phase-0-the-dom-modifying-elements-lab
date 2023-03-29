337d99bbfee32dac22979c00ed244a63bbbf6e49
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-03-29 01:47:57 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ document.querySelector("main#main").remove();
 //Create your new element here and assign it to newHeader 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = " Dominic is the champion!";// my name
+newHeader.innerHTML = " Patience is the champion!";// my name
 newHeader.className = "victory";
